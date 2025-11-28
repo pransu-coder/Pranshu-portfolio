61995e36ad9438d70c52a9246258ab8878f16fd6
pransu-coder <pransu145@gmail.com>
2025-11-28 16:16:32 +0530

    Update projects-data.ts

--- lib/projects-data.ts
+++ lib/projects-data.ts
@@ -6,7 +6,7 @@ export const projectsData = {
       {
         title: 'GoldenBangle(New Project)',
         desc: 'Designed and developed the complete frontend with clean & responsive UI.',
-        visual: '/pixlpe.jpg',
+        visual: '/gold.jpg',
         bg: 'bg-emerald-300 dark:bg-emerald-600',
         links: [
           { text: 'Frontend UI', url: 'https://pranshu-golden-bangle.vercel.app/' },
